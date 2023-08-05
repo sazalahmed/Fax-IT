1316bbadd68c9c0a9a0166a2815f281ffa3c956f
Alamin-Coding <alamin.codelover@gmail.com>
2023-08-05 19:53:03 +0600

    Topbar, Navbar, Footer Added without responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(function () {
     
     "use strict";
+    // Get FullYear
+    let year = document.getElementById("year");
+    let getYear = new Date().getFullYear();
+
+    year.innerText = getYear;
     
     
 
