70141cd8118f1736eb41200ef57b99479d98f539
Alamin-Coding <alamin.codelover@gmail.com>
2023-08-06 20:28:49 +0600

    features section add

--- js/main.js
+++ js/main.js
@@ -6,6 +6,14 @@ $(function () {
     let getYear = new Date().getFullYear();
     year.innerText = getYear;
 
+    // Section Heading Background Stroke Text
+    // let strokeText = Array.from(document.querySelectorAll(".tf__text_stroke"));
+    // let getHeadingText = Array.from(document.querySelectorAll(".tf_section_heading h2"));
+
+    // console.log(strokeText, getHeadingText);
+
+
+ 
    
     
     
