849f1bd74b151c846968daa87f8f9af807f3d4d9
freelancersazal <freelancersazal@gmail.com>
2023-08-08 01:11:34 +0600

    home page done

--- js/main.js
+++ js/main.js
@@ -1,37 +1,165 @@
 $(function () {
-    
+
     "use strict";
-    // Get FullYear
-    let year = document.getElementById("year");
-    let getYear = new Date().getFullYear();
-    year.innerText = getYear;
-
-    // Section Heading Background Stroke Text
-    // let strokeText = Array.from(document.querySelectorAll(".tf__text_stroke"));
-    // let getHeadingText = Array.from(document.querySelectorAll(".tf_section_heading h2"));
-
-    // console.log(strokeText, getHeadingText);
 
 
- 
-   
-    
-    
+    //=========MENU FIX JS=========   
+    if ($('.main_menu').offset() != undefined) {
+        var navoff = $('.main_menu').offset().top;
+        $(window).scroll(function () {
+            var scrolling = $(this).scrollTop();
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+            if (scrolling > navoff) {
+                $('.main_menu').addClass('menu_fix');
+            } else {
+                $('.main_menu').removeClass('menu_fix');
+            }
+        });
+    }
+
+
+    //===venobox js===
+    $('.venobox').venobox();
+
+
+    //======menu search js======
+    $(".search_icon").on("click", function () {
+        $(".menu_search").addClass("show_search");
+    });
+
+    $(".close_search").on("click", function () {
+        $(".menu_search").removeClass("show_search");
+    });
+
+
+    //*==========ISOTOPE==============
+    var $grid = $('.grid').isotope({});
+
+    $('.tf__projects_filter').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+
+    //active class
+    $('.tf__projects_filter button').on("click", function (event) {
+
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        event.preventDefault();
+    });
+
+
+    //=========TEAM SLIDER=========   
+    $('.team_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="far fa-long-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="far fa-long-arrow-left prevArrow"></i>',
+
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+
+
+    //======TESTIMONIAL SLIDER======   
+    $('.testi_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="far fa-long-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="far fa-long-arrow-left prevArrow"></i>',
+    });
+
+
+    //======BRAND SLIDER======   
+    $('.marquee_animi').marquee({
+        speed: 100,
+        gap: 0,
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: true,
+        pauseOnHover: true
+    });
+
+
+    //====SCROLL BUTTON========  
+    $('.scroll_btn').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0,
+        },);
+    });
+
+    $(window).on('scroll', function () {
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > 300) {
+            $('.scroll_btn').fadeIn();
+        }
+
+        else {
+            $('.scroll_btn').fadeOut();
+        }
+    });
+
+
+
+
+
+
+
 
 });
